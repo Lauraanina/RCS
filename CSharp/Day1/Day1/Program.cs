f35afc77ab293c1ff856e3a1dd348b382b23fc51
Lauraanina <42405532+Lauraanina@users.noreply.github.com>
2018-09-03 19:48:48 +0300

    day1 created

---
+++ CSharp/Day1/Day1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int veselsSkaitlis = 5;
+            double arKomatu = 4.5;
+            String simboluVirkne = "definējas šādi";
+            char simbols = 'g';
+            bool patiessVaiNe = true;
+
+            veselsSkaitlis = 6;
+            simboluVirkne = Console.ReadLine();
+            Console.WriteLine(simboluVirkne);
+
+            Console.ReadLine();
+        }
+    }
+}
